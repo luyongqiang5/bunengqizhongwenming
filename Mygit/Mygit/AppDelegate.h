315175b06e680c4f7d309ddcf66beab9b9e7aa34
luyongqiang5 <2545123654@qq.com>
2016-01-13 14:23:54 +0800

    刚创建的项目

---
+++ Mygit/Mygit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mygit
+//
+//  Created by ma c on 16/1/13.
+//  Copyright © 2016年 卢永强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
