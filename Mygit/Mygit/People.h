e3648c2ab24f7a3360d1523b63cdbb9bad4677a6
luyongqiang5 <2545123654@qq.com>
2016-01-13 14:31:19 +0800

    加一个People类

---
+++ Mygit/Mygit/People.h
@@ -0,0 +1,13 @@
+//
+//  People.h
+//  Mygit
+//
+//  Created by ma c on 16/1/13.
+//  Copyright © 2016年 卢永强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface People : UIViewController
+
+@end
---
+++ Mygit/Mygit/People.m
@@ -0,0 +1,13 @@
+//
+//  People.m
+//  Mygit
+//
+//  Created by ma c on 16/1/13.
+//  Copyright © 2016年 卢永强. All rights reserved.
+//
+
+#import "People.h"
+
+@implementation People
+
+@end
